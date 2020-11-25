63ba83849e096574d3ee223c30309a4d42b78f46
huytang <huytt@youmed.vn>
2020-11-25 17:17:46 +0700

    Push source code

---
+++ src/test/java/vn/app/news/newsapi/NewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.app.news.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
